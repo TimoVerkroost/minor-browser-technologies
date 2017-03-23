8b762978f6fec61c26696d4fd14c139ff49d89a9
Timo Verkroost <timo.verkroost@gmail.com>
2017-03-23 15:18:14 +0100

    Init const feature

---
+++ 2.1-assignment-feature-detection/demos/js/es5.js
@@ -0,0 +1,2 @@
+var support = document.getElementById("support");
+support.innerHTML = "This browser doesn't support ES6 and it's fallback will be ES5. So the const deceleration won't work.";
---
+++ 2.1-assignment-feature-detection/demos/js/es6.js
@@ -0,0 +1,2 @@
+const support = document.getElementById("support");
+support.innerHTML = "This browser does support ES6 so the const deceleration works here.";
