d18e07ec3cb56322882d385f7915a4500b6db07d
Timo Verkroost <timo.verkroost@gmail.com>
2017-03-23 15:59:25 +0100

    Update const because safari bugs

--- 2.1-assignment-feature-detection/demos/js/es5.js
+++ 2.1-assignment-feature-detection/demos/js/es5.js
@@ -1,5 +1,5 @@
 (function () {
     var supportChecker = document.getElementById("support");
     console.log(supportChecker);
-    //supportChecker.innerHTML = "This browser doesn't support ES6 and it's fallback will be ES5. So the const deceleration won't work.";
+    supportChecker.innerHTML = "This browser doesn't support ES6 and it's fallback will be ES5. So the const deceleration won't work.";
 }());
--- 2.1-assignment-feature-detection/demos/js/es6.js
+++ 2.1-assignment-feature-detection/demos/js/es6.js
@@ -1,5 +1,5 @@
 (function () {
     const supportChecker = document.getElementById("support");
     console.log(supportChecker);
-    //supportChecker.innerHTML = "This browser does support ES6 so the const deceleration works here.";
+    supportChecker.innerHTML = "This browser does support ES6 so the const deceleration works here.";
 }());
