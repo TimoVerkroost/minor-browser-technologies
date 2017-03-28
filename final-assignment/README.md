b38354ae6008eec435203027a9f25a6dde9d66dc
Timo Verkroost <timo.verkroost@gmail.com>
2017-03-28 14:54:45 +0200

    Added pure HTML

--- final-assignment/README.md
+++ final-assignment/README.md
@@ -2,14 +2,19 @@
 **Use case:** I want to save my favorite t-shirts with nerdy-texts, and the next time I visit the website I want to see my saved items.
 
 ## Description feature
+The user visit the website where he/she can assemble a t-shirt with some "nerdy" text. But when the user leaves the website and came back again the saved text must be recovered, so the user won't have to fill in the text fields again.
 
 ## Browser support
 
 ## Accessibility issues
 
 ## Structure
+The base of my application is server-side where some variables are saved that makes us less dependent of the user it's browser. To do this I used: NodeJS with an Express server + Handlebars templating. This will serve a static HTML page but I can handle some input server-side so I can make it more personal for the user.
 
 ### HTML
+First I started with the plain HTML, I did this with a basic form tag and some input fields. Those input fields will be saved on the server so we don't need any javascript to store the data client-side. With this form the user can add his/her name and 1-3 "Nerdy" texts to save or to create a shirt, the name and minimal 1 "Nerdy" text is required to pass the form (client-side validation).
+  
+  When the user decide to create the shirt he/she can choice between some sizes and also that input will be displayed as feedback to the user.
 
 ### CSS
 
--- final-assignment/app/routes/index.js
+++ final-assignment/app/routes/index.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var LocalStorage = require('node-localstorage').LocalStorage;
+var shirtTextStorage = new LocalStorage('./storage/shirt-txt');
+
+
+var pageOptions =
+  {
+    title: "Final assignment"
+  };
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', pageOptions);
+});
+
+router.post('/search', function(req, res) {
+  var name = req.body.name;
+  var dataSaved = JSON.parse(shirtTextStorage.getItem(name));
+
+  res.locals.form = req.body;
+  res.locals.dataSaved = dataSaved;
+  if (dataSaved !== null) {
+    res.render('results', pageOptions);
+  } else {
+    res.render('index', pageOptions);
+  }
+});
+
+router.post('/saved', function(req, res) {
+  var name = req.body.name;
+  var shirtText1 = req.body.shirtText1;
+  var shirtText2 = req.body.shirtText2;
+  var shirtText3 = req.body.shirtText3;
+  var myShirts = { shirt1: shirtText1, shirt2: shirtText2, shirt3: shirtText3 };
+
+  shirtTextStorage.setItem(name, JSON.stringify(myShirts));
+
+  var dataSaved = JSON.parse(shirtTextStorage.getItem(name));
+
+  res.locals.form = req.body;
+  res.locals.dataSaved = dataSaved;
+  if (dataSaved !== null) {
+    res.render('results', pageOptions);
+  } else {
+    res.render('index', pageOptions);
+  }
+});
+
+router.post('/create', function(req, res) {
+  res.locals.create = req.body;
+  res.render('create', pageOptions);
 });
 
 module.exports = router;
